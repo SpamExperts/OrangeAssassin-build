0d3ad3bf401ac696fb89f3e817c1f817ed855fd6
Andrei Romila <romila.andrei@gmail.com>
2017-03-23 10:52:19 +0100

    Specify version in build.sh

--- debian/build.sh
+++ debian/build.sh
@@ -6,16 +6,29 @@
 
 set -e
 
-PYTHON_VERSION=2
+#Supports python2.7,python3.4,python3.5
+PYTHON_MAJV=2
+PYTHON_MINV=4
 
-apt-get update; apt-get install -y equivs python-dev dh-systemd python3.4-dev libjpeg-dev libghc-zlib-dev
+apt-get update; apt-get install -y libghc-zlib-dev
 
-pushd python$PYTHON_VERSION
+pushd python$PYTHON_MAJV
 test -e OrangeAssassin || git clone https://github.com/SpamExperts/OrangeAssassin
 sed -i OrangeAssassin/setup.py -e '/tests_require=test_requirements/d'
 mv OrangeAssassin/* ./
+rm OrangeAssassin/ -r
+
+if [ $PYTHON_MAJV -eq 3 ]
+then 
+    if [ $PYTHON_MINV -eq 5 ]
+    then
+        sed -i 's/3\.4/3\.5/g' debian/control
+    else
+        sed -i 's/3\.5/3\.4/g' debian/control
+fi
 
 #Build the deb package
 dpkg-buildpackage -us -uc                   #remove test require from setup.py
-popd
+cd python$PYTHON_MAJV
+rm doc/ oa/ requirements/ scripts/ tests/ -r
 
